21331965e25ece7449a69155e37521554d7692f9
uaysev <utkua557@gmail.com>
2023-10-27 10:36:24 -0500

    err fix

--- routers/CarRouter.js
+++ routers/CarRouter.js
@@ -1,9 +1,7 @@
 const controller = require("../controller/CarController")
 const express = require("express")
 const router = express.Router();
-const multer = require('multer');
 
-const upload = multer({ dest: 'public/img' });
 
 router.get("/", controller.gettAll)
 router.get("/:vin", controller.getByVin)
